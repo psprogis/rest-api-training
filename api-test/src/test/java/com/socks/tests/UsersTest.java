2b3e2ad84ac25204e33259c21ebdb83b0dab82dd
Pavlo Sprogis <Pavlo.Sprogis@harmonicinc.com>
2019-06-14 20:53:57 +0300

    add UserPayload class

--- api-test/src/test/java/com/socks/tests/UsersTest.java
+++ api-test/src/test/java/com/socks/tests/UsersTest.java
@@ -1,8 +1,17 @@
 package com.socks.tests;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class UsersTest {
+
+    @BeforeClass
+    public void setUp() {
+        RestAssured.baseURI = "http://10.47.114.42/";
+    }
+
     @Test
     public void testCanRegisterNewUser() {
     }
