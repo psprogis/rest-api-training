9509b9e1565a9512f4d0d14a130a2c42e93d3e80
pavel sprogis <pavel.sprogis@gmail.com>
2019-06-19 13:03:27 +0300

    use logging variable to enable logging

--- api-test/src/main/java/com/socks/api/services/ApiService.java
+++ api-test/src/main/java/com/socks/api/services/ApiService.java
@@ -1,12 +1,31 @@
 package com.socks.api.services;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.Filter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class ApiService {
     protected RequestSpecification setup() {
         return RestAssured
-                .given().contentType(ContentType.JSON).log().all();
+                .given().contentType(ContentType.JSON)
+                .filters(getFilters());
+    }
+
+    private List<Filter> getFilters() {
+        Boolean enableLog = Boolean.valueOf(System.getProperty("logging", "true"));
+
+        if (enableLog) {
+            return Arrays.asList(new RequestLoggingFilter(), new ResponseLoggingFilter());
+        }
+
+        return Collections.emptyList();
     }
 }
