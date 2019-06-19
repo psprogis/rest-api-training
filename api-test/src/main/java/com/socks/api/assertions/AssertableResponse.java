b28f7404ac5b16a5618bd770ecb0d77e32906226
pavel sprogis <pavel.sprogis@gmail.com>
2019-06-19 13:03:04 +0300

    add UserRegistrationResponse

--- api-test/src/main/java/com/socks/api/assertions/AssertableResponse.java
+++ api-test/src/main/java/com/socks/api/assertions/AssertableResponse.java
@@ -1,17 +1,26 @@
 package com.socks.api.assertions;
 
 import com.socks.api.conditions.Condition;
+import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class AssertableResponse {
 
-    private final Response register;
+    private final Response response;
 
     public AssertableResponse shouldHave(Condition condition) {
-        condition.check(register);
+        condition.check(response);
 
         return this;
+    }
+
+    public <T> T asPojo(Class<T> tClass) {
+        return response.as(tClass);
+    }
+
+    public Headers headers() {
+        return response.getHeaders();
     }
 }
