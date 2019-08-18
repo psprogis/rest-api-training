667285afbf97c60af1ac6966bd4090b57e56bedb
pavel sprogis <pavel.sprogis@gmail.com>
2019-08-18 21:26:58 +0300

    add new pages

---
+++ ui-test/src/main/java/com/socks/ui/CatalogPage.java
@@ -0,0 +1,27 @@
+package com.socks.ui;
+
+import com.codeborne.selenide.Selenide;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.page;
+
+public class CatalogPage {
+    public static CatalogPage open() {
+        Selenide.open("/category.html");
+
+        return page(CatalogPage.class);
+    }
+
+    public CatalogPage addItemByIndex(int index) {
+        $$("#products .product .buttongs a.btn-primary").get(index).click();
+
+        return this;
+    }
+
+    public ShoppingCartPage goToCart() {
+        $("#numItemsInCart").click();
+
+        return page(ShoppingCartPage.class);
+    }
+}
