48dc8905a39c76d1cd9e6c2404a1dbc1b12adece
pavel sprogis <pavel.sprogis@gmail.com>
2019-08-18 14:35:59 +0300

    first login test

--- ui-test/src/test/java/com/socks/ui/tests/TestLogin.java
+++ ui-test/src/test/java/com/socks/ui/tests/TestLogin.java
@@ -1,18 +1,19 @@
-package com.socks.ui.test;
+package com.socks.ui.tests;
 
 import org.testng.annotations.Test;
 
 import com.socks.api.payloads.UserPayload;
 import com.socks.api.services.UserApiService;
+import com.socks.ui.LoggedUserPage;
 import com.socks.ui.MainPage;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.socks.api.conditions.Conditions.statusCode;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 
-public class TestLogin {
+public class TestLogin extends BaseUITest {
 
     private final UserApiService userApiService = new UserApiService();
-
 
     @Test
     public void userCanLoginWithValidCredentials() {
@@ -21,11 +22,14 @@ public class TestLogin {
                                .password("abc")
                                .email("foo@gmail.com");
 
-
         userApiService.registerUser(user)
                       .shouldHave(statusCode(200));
 
         MainPage.open()
                 .loginAs(user.username(), user.password());
+
+        LoggedUserPage loggedUserPage = at(LoggedUserPage.class);
+        loggedUserPage.logoutBtn().shouldHave(text("logout"));
+        loggedUserPage.userName().shouldHave(text("Logged in as"));
     }
 }
